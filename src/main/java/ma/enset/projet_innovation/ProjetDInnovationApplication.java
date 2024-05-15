7dc7170a6655766174c2e67dbbd61d9e6c19ffc6
BrahimNAJI2002 <brahim.naji1-etu@etu.univh2c.ma>
2024-05-15 19:25:31 +0100

    first commit

---
+++ src/main/java/ma/enset/projet_innovation/ProjetDInnovationApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.projet_innovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDInnovationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetDInnovationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/projet_innovation/ProjetDInnovationApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.projet_innovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDInnovationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
